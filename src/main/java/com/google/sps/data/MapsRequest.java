f4a7e41d21c88b1a7863e7b24e683829d754099b
anak <ana.kapros@yahoo.ro>
2020-08-20 18:15:46 +0000

    add MapsRequest class to handle requests to maps APIs and Point class to represent pairs of latitude and longitude

---
+++ src/main/java/com/google/sps/data/MapsRequest.java
@@ -0,0 +1,45 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.sps.data;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.errors.ApiException;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeoApiContext.Builder;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
+
+public class MapsRequest {
+  private static GeoApiContext geoApiContext = null;
+  public static GeoApiContext getGeoApiContext() {
+    if (geoApiContext == null) {
+      geoApiContext = new GeoApiContext
+          .Builder()
+          .apiKey("")
+          .build();
+    }
+    return geoApiContext;
+  }
+  public static LatLng getLocationFromAddress(String address) {
+    try {
+      GeocodingResult[] results =  GeocodingApi.geocode(getGeoApiContext(), address).await();
+      Gson gson = new GsonBuilder().setPrettyPrinting().create();
+      return results[0].geometry.location;
+    } catch(Exception e) {
+      System.out.println(e.getMessage());
+      return null;
+    }
+  }
+}
