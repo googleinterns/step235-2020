4b8a8e5f5b5d8a6f93b642f3b491a3a792d52ea6
anak <ana.kapros@yahoo.ro>
2020-08-24 12:17:16 +0000

    add LibraryPoint class that represents a library during a delivery journey

---
+++ src/main/java/com/google/sps/data/LibraryPoint.java
@@ -0,0 +1,55 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.google.sps.data;
+
+import com.google.maps.model.LatLng;
+import com.google.sps.data.Point;
+import com.google.sps.data.MapsRequest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Class that represents a library point in a delivery journey using its latitude and longitude.
+ */
+public class LibraryPoint extends Point {
+  ArrayList<String> booksToBorrow;
+
+  public LibraryPoint(double latitude, double longitude) {
+    super(latitude, longitude);
+    booksToBorrow = new ArrayList<>();
+  }
+
+  public LibraryPoint(String address) {
+    super(address);
+    booksToBorrow = new ArrayList<>();
+  }
+
+  /**
+   * Adds the Google Books ids of the books from bookIdsString to the books that must be borrowed
+   * from this library during current delivery
+   */
+  public void addBooks(String bookIdsString) {
+    String[] bookIds = bookIdsString.split("&");
+    booksToBorrow.addAll(Arrays.asList(bookIds));
+  }
+
+  /**
+   * Returns the Google Books ids of the books that must be borrowed from this library during
+   * current delivery.
+   */
+  public ArrayList<String> getBooksToBorrow() {
+    return booksToBorrow;
+  }
+}
