543c67e0cbc7a80497ae5214b3fd96763d5fbd86
anak <ana.kapros@yahoo.ro>
2020-09-05 11:26:51 +0000

    implement methods in BooksManager.java

--- src/main/java/com/google/sps/data/BooksManager.java
+++ src/main/java/com/google/sps/data/BooksManager.java
@@ -26,37 +26,62 @@ import java.lang.Number;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Class used for managing the stock of books. 
- */
 public class BooksManager {
   /**
    * Returns true if there is at least one library that has the book with bookId in stock and false
    * otherwise.
    */
   protected static boolean isBookInStore(String bookId) {
-    throw new UnsupportedOperationException();
+    List<Entity> results = getLibrariesForBook(bookId, /** setLimit = */true);
+    return (results.size() > 0);
   }
 
   /**
    * Returns the Key of book bookId from datastore or null if there is no book with bookId.
    */
   private static Key getBookKeyFromDatastore(String bookId) {
-    throw new UnsupportedOperationException("Method will return the Key of book with id bookId");
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query bookQuery = new Query("Book")
+        .setFilter(new Query.FilterPredicate("bookId", Query.FilterOperator.EQUAL, bookId))
+        .setKeysOnly();
+    PreparedQuery result = datastore.prepare(bookQuery);
+    if (result.countEntities(FetchOptions.Builder.withDefaults()) == 0) {
+      return null;
+    }
+    return result.asSingleEntity().getKey();
   }
-
+  
   /** 
    * Returns a List of "Library" Entity objects that have bookId in stock.
    */
   protected static List<Entity> getLibrariesForBook(String bookId, boolean setLimit) {
-    throw new UnsupportedOperationException("Method will return a List of LibraryEntities that contain bookId.");
+    Key bookKey = getBookKeyFromDatastore(bookId);
+    if (bookKey == null) {
+      // bookId is not in the datastore
+      // TODO[ak47na]: handle case when bookId is not valid
+      return new ArrayList<>();
+    }
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query libraryQuery = new Query("Library").setAncestor(bookKey)
+        .setFilter(new Query.FilterPredicate("stock", Query.FilterOperator.NOT_EQUAL, 0));
+    return datastore.prepare(libraryQuery).asList(FetchOptions.Builder.withDefaults());
   }
 
   /** 
-   * Removes one occurence for each book in bookIds from library. This method is called after an
-   * order for renting books in bookIds from library is added to datastore.  
+   * Updates the stock of library in datastore by removing one piece for each book in bookIds. This
+   * method is called after an order for renting books in bookIds from library is added to datastore.  
    */
   protected static void removeBooksFromLibrary(LibraryPoint library, List<String> bookIds) {
-    throw new UnsupportedOperationException("Method will remove bookIds from library in datastore.");
+    for (String bookId: bookIds) {
+      Key bookKey = getBookKeyFromDatastore(bookId);
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      
+      Query libraryQuery = new Query("Library").setAncestor(bookKey)
+          .setFilter(new Query.FilterPredicate("libraryId", Query.FilterOperator.EQUAL, (int)library.getLibraryId()));
+      
+      Entity result = datastore.prepare(libraryQuery).asSingleEntity();
+      result.setProperty("stock", ((Number) result.getProperty("stock")).intValue() - 1);
+    }
   }
 }
