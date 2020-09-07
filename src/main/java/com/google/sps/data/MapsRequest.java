2edfc817916e33f4a947e4d76f6ddf6305a9f690
anak <ana.kapros@yahoo.ro>
2020-09-07 07:53:08 +0000

    remove wildcard import and add newline eof

--- src/main/java/com/google/sps/data/MapsRequest.java
+++ src/main/java/com/google/sps/data/MapsRequest.java
@@ -19,7 +19,11 @@ import com.google.maps.errors.ApiException;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeoApiContext.Builder;
 import com.google.maps.GeocodingApi;
-import com.google.maps.model.*;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.TravelMode;
+import com.google.maps.model.LatLng;
 import java.io.IOException;
 
 /**
